85acd99260c9a6573f161252e948fcc98fb29b89
Usuario <Usuario@CPD02-M07>
2024-04-30 10:46:32 -0300

    Adicionar arquivos de projeto.

---
+++ CosoleArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosoleArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nome = { "joão", "pedro", "maria" };
+
+            Console.WriteLine("Digite um numero de 0 a 2: ");
+            int i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O nome selecionado foi: " + nome[i]);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
